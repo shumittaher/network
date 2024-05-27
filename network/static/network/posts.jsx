2f105b26860697ecd9919cedfba59a0fc667716a
shumit taher <shumitt@gmail.com>
2024-05-27 12:18:05 +0600

    fetching done

--- network/static/network/posts.jsx
+++ network/static/network/posts.jsx
@@ -1,20 +1,28 @@
-
-
 function Post_list() {
     
     const [posts, setPosts] = React.useState([]);
-
-    fetch('/post_supply')
-    .then(response=>response.json())
-    .then(results=>setPosts(results))
-
+    
+    React.useEffect(() => {
+        fetch('/post_supply')
+            .then(response=>response.json())
+            .then(results=>setPosts(results))
+    }, []);
+    
+    console.log(posts)
+    
     return (
-        posts.map(post=>{
         <div>
-            `${post}`
+
+            {posts.map(post => {
+
+            return <div key={post.id || post._id}>
+                <h2>{post.post_title}</h2>
+                <p>{post.post_text}</p>
+            </div>
+
+        })}
         </div>
-        })
-)
+    )
 }
 
 ReactDOM.render(<Post_list />, document.querySelector("#post_list"));
