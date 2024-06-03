dd894d1f0d51dec113cc1810f8c5f3192fef1e68
shumit taher <shumitt@gmail.com>
2024-06-03 13:46:21 +0600

    comments

--- network/urls.py
+++ network/urls.py
@@ -9,11 +9,12 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("post_supply", views.post_supply, name="post_supply"),
-    path("followed_post/<str:follow>", views.post_supply, name="follwed_post"),
-    path("followers_supply/<int:user_id>/<str:followers>", views.followers_supply, name="followers_supply"),
-    path("fetch_post/<int:post_id>", views.post_supply, name="fetch_post"),
-    path("like_route", views.like_route, name="like_route"),
     path("profile/<int:user_id>", views.profile, name="profile"),
-    path("follow_route", views.follow_route, name="follow_route")
+    #json requests
+    path("post_supply", views.post_supply),
+    path("followed_post/<str:follow>", views.post_supply),
+    path("fetch_post/<int:post_id>", views.post_supply),
+    path("followers_supply/<int:user_id>/<str:followers>", views.followers_supply),
+    path("like_route", views.like_route),
+    path("follow_route", views.follow_route)
 ]
